3903a7398aceaf622c2839163912be6c1c48bdc4
Bert Witzel <bwitzel82@gmail.com>
2021-06-12 14:23:45 -0400

    fix a lot of things

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
-// global variables
-const btn = document.querySelectorAll('button[type="button"]');
-
 // flip button on dropdown
-btn.forEach(button => button.addEventListener('click', () => {
-    // button.style.transform = 'rotate(180deg)';
-    if(button.classList.contains('flip')){
-        button.classList.remove('flip');
-    } else {
-        button.classList.add('flip');
-    };
-    if(button.parentNode.firstElementChild.classList.contains('strong-font')){
-        button.parentNode.firstElementChild.classList.remove('strong-font');
-    } else {
-        button.parentNode.firstElementChild.classList.add('strong-font');
-    };
+const collapse = document.querySelectorAll('.collapse');
+
+collapse.forEach(div => div.addEventListener('show.bs.collapse', function () {
+    div.parentNode.firstElementChild.firstElementChild.nextElementSibling.classList.add('flip');
+    div.parentNode.firstElementChild.firstElementChild.firstElementChild.classList.add('strong-font');
+}));
+
+collapse.forEach(div => div.addEventListener('hide.bs.collapse', function () {
+    div.parentNode.firstElementChild.firstElementChild.nextElementSibling.classList.remove('flip');
+    div.parentNode.firstElementChild.firstElementChild.firstElementChild.classList.remove('strong-font');
 }));
